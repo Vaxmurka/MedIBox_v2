dcbc6e3dc6ad94b42e1a0ee136c92f1ed2a2a7c8
Vaxmurka <nikita.rum07@yandex.ru>
2024-08-18 17:47:27 +0400

    Шаг к отправке и парсингу json файлов между Django и ESP

--- device/urls.py
+++ device/urls.py
@@ -6,4 +6,5 @@ app_name = 'device'
 urlpatterns = [
     # path('<slug:slug>', views.patient, name='patient_detail'),  # new
     path('device/', views.index, name='device'),
+    path('send-json/', views.send_json, name='send_json'),
 ]
--- device/views.py
+++ device/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def send_json(request):
+    # Создайте JSON данные для отправки
+    data = {
+        "message": "Hello from Django",
+        "value": 42,
+        "type": "test message",
+        "length": 5,
+        "name": "Hello ESP32"
+    }
+    return JsonResponse(data)
 
 
 def index(request):
