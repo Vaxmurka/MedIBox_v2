3121382df6fd49a8b91a4ca7e984564c634378da
Vaxmurka <nikita.rum07@yandex.ru>
2024-08-18 21:26:51 +0400

    Исправленна логика смяна темы. Теперь пациенты привязаны к пользователю. Исправленны мелкие ошибки

--- patients/views.py
+++ patients/views.py
@@ -8,15 +8,10 @@ from django.contrib.auth.decorators import login_required
 from users.models import User
 
 
-def allpatients(request, group_slug=None):
-    group = None
+def allpatients(request):
     groups = Groups.objects.all()
-    patients = Patient.objects.all()
-    # role: int = User.get_role(request.user) #request.user
-
-    if group_slug:
-        group = get_object_or_404(Groups, slug=group_slug)
-        patients = Patient.objects.all()
+    patients = Patient.objects.filter(user_id=request.user.id)
+    group = groups.values_list()[0][1]
 
     context = {
         'title': 'MedIBox - Пользователи',
@@ -24,16 +19,9 @@ def allpatients(request, group_slug=None):
         'groups': groups,
         'patients': patients,
         'patient': 'patient',
-        # 'role': role,
     }
     return render(request, 'allPatients.html', context)
 
-
-# def patient_detail(request, id, slug=1):
-#     patient = get_object_or_404(Patient, id=id, slug=slug, available=True)
-#
-#     context = {'patient': patient}
-#     return render(request, 'patient.html', context)
 
 def patient_detail(request, patient_slug):
     patient = list(Patient.objects.filter(slug=patient_slug).values_list()[0])
