51816c9014347dee63c4dbf542e712006394252f
Vaxmurka <nikita.rum07@yandex.ru>
2024-08-26 09:40:40 +0400

    Сбор данных в один json файл

--- device/urls.py
+++ device/urls.py
@@ -6,5 +6,5 @@ app_name = 'device'
 urlpatterns = [
     # path('<slug:slug>', views.patient, name='patient_detail'),  # new
     path('device/', views.index, name='device'),
-    # path('send_json/', views.send_json, name='send_json'),
+    path('send_json/', views.send_json, name='send_json'),
 ]
--- device/views.py
+++ device/views.py
@@ -5,36 +5,7 @@ from users.models import User
 import json
 
 
-# def send_json():
-#     # Создайте JSON данные для отправки
-#     users = User.objects.all()
-#     data = []
-#
-#     for user in users:
-#         patients = Patient.objects.filter(user=user)
-#
-#         for patient in patients:
-#             group = patient.groups
-#             water = patient.portion_of_water
-#             id = patient.fingerprint
-#             name = patient.first_name + ' ' + patient.username
-#
-#             schedules = Schedule.objects.filter(patient=patient)
-#
-#             data_patient = {
-#                 'Пользователь': name,
-#                 'id': id,
-#                 'Группа': group,
-#                 'Количество воды': water,
-#                 'Расписание': schedules,
-#                 'Опекун': user.username
-#             }
-#             data.append(data_patient)
-#     print(data)
-#     return data
-
-def send_json():
-    # Создайте JSON данные для отправки
+def send_json(request):
     users = User.objects.all()
     data = []
 
@@ -55,49 +26,52 @@ def send_json():
 
                 data_pill = {}
                 pills = Pills.objects.filter(taking=taking)
-                # pill = Pills.objects.filter(t)
                 for pill in pills:
                     name_p = pill.name
                     container = pill.container
                     data_pill = {
-                        'Название': name_p,
-                        'Контейнер': container,
+                        "Название": name_p,
+                        "Контейнер": container,
                     }
 
                 data_taking = {
-                    'таблетка': data_pill,
-                    'количество': quantity_pills,
-                    'время': time,
-                    'дни': {
-                        'понедельник': taking.monday,
-                        'вторник': taking.tuesday,
-                        'среда': taking.wednesday,
-                        'четверг': taking.thursday,
-                        'пятница': taking.friday,
-                        'суббота': taking.saturday,
-                        'воскресенье': taking.sunday,
+                    "таблетка": data_pill,
+                    "количество": quantity_pills,
+                    # "время": time,
+                    "дни": {
+                        "понедельник": taking.monday,
+                        "вторник": taking.tuesday,
+                        "среда": taking.wednesday,
+                        "четверг": taking.thursday,
+                        "пятница": taking.friday,
+                        "суббота": taking.saturday,
+                        "воскресенье": taking.sunday,
                     },
                 }
                 data_schedule.append(data_taking)
-                print(data_taking)
 
             data_patient = {
-                'Пользователь': name,
-                'id': id,
-                'Группа': group,
-                'Количество воды': water,
-                'Расписание': data_schedule,
-                'Опекун': user.username
+                "Пользователь": name,
+                "id": id,
+                # "Группа": group,
+                "Количество воды": water,
+                "Расписание": data_schedule,
+                "Опекун": user.username
             }
             data.append(data_patient)
-    print(data)
-    return data
+    # print(data)
+    json_data = json.dumps(data, ensure_ascii=False)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        f.write(json_data)
+    print(json_data)
+    return JsonResponse(json_data, safe=False)
+    # return json_data
 
 
 def index(request):
     context = {
         'title': 'MedIBox - Устройства',
         'device': 'device',
-        'data': send_json()
+        # 'data': send_json()
     }
     return render(request, "device.html", context)
